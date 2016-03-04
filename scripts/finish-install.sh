ab243e8a7c9bca988806d04fec0df8a69d322754
R. S. Doiel <rsdoiel@gmail.com>
2016-03-04 11:48:05 -0800

    Yippee, E-Prints comes up

--- scripts/finish-install.sh
+++ scripts/finish-install.sh
@@ -3,7 +3,7 @@
 #
 
 # Install E-Prints
-sudo rpm -ivh http://rpm.eprints.org/rpm-eprints-org-key-1-1.noarch.rpm
-sudo rpm -ivh http://rpm.eprints.org/eprints/noarch/rpm-eprints-org-1-1.noarch.rpm
-sudo yum install eprints # 3.3.x and later
+#sudo rpm -ivh http://rpm.eprints.org/rpm-eprints-org-key-1-1.noarch.rpm
+#sudo rpm -ivh http://rpm.eprints.org/eprints/noarch/rpm-eprints-org-1-1.noarch.rpm
+#sudo yum install eprints # 3.3.x and later
 
